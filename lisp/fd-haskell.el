87bde9e1600130ebd2a538e6b8684f0f764f308c
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-12-28 01:40:09 +0200

    Haskell

---
+++ lisp/fd-haskell.el
@@ -0,0 +1,7 @@
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'fd-haskell-mode)
+(defun fd-haskell-mode ()
+  (setq comment-auto-fill-only-comments nil))
+(provide 'fd-haskell)
