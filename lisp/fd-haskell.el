4dbb8d766e692e97c47169f939e3edcef9cdcee7
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2016-10-10 12:32:14 +0100

    Haskell error parsing for compilation mode

--- lisp/fd-haskell.el
+++ lisp/fd-haskell.el
@@ -13,13 +13,32 @@
   (interactive)
   (if (not (region-active-p)) (call-interactively haskell-process-load-file)
     (let* ((tmp (concat (make-temp-file "haskell-region") ".hs"))
-          (haskell-session (haskell-session))
-          (cmd (format "load \"%s\"" (replace-regexp-in-string
-                                      "\""
-                                      "\\\\\""
-                                      tmp))))
+           (haskell-session (haskell-session))
+           (cmd (format "load \"%s\"" (replace-regexp-in-string
+                                       "\""
+                                       "\\\\\""
+                                       tmp))))
       (write-region (region-beginning) (region-end) tmp)
       (haskell-process-file-loadish cmd nil buf)
       (delete-file tmp))))
+
+(defvar hs-compilation-error-regex-alist
+  ;; REGEX FILE-GROUP LINE-GROUP COLUMN-GROUP ERROR-TYPE LINK-GROUP
+  '((haskell-error
+     "^\\(\\(.*\\.l?hs\\):\\([0-9]*\\):\\([0-9]*\\)\\): error:$"
+     2 3 4 2 1)))
+
+
+;; Add hs-compilation-error-regex-alist to the appropriate lists. If
+;; the var was updated don't keep adding, edit the result.
+(require 'compile)
+(dolist (el hs-compilation-error-regex-alist)
+  (unless (memq (car el) compilation-error-regexp-alist)
+    (add-to-list 'compilation-error-regexp-alist (car el)))
+
+  (let ((ael (assq (car el) compilation-error-regexp-alist-alist)))
+    (if ael (setf (cdr ael) (cdr el))
+      (add-to-list 'compilation-error-regexp-alist-alist el))))
+
 
 (provide 'fd-haskell)
