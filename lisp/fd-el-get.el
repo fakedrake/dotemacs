310c9b277caf0f1dc6c186363119636faee8109b
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2017-01-19 17:01:34 +0000

    Maxima

--- lisp/fd-el-get.el
+++ lisp/fd-el-get.el
@@ -15,6 +15,7 @@
 
 (setq my:el-get-packages
       '(;; Dependencies from who-kows-where
+        imaxima
         irony-mode
         tree-mode
         dictionary
--- lisp/fd-maxima.el
+++ lisp/fd-maxima.el
@@ -1,3 +1,2 @@
-(add-to-list 'load-path "/usr/local/Cellar/maxima/5.38.1/share/maxima/5.38.1/emacs")
 (require 'imaxima)
 (provide fd-maxima)
