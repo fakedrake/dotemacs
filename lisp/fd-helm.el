89af2cdeb5c3b858e2520ddef4b1ad1ff31a8b7e
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2018-12-12 13:15:49 +0000

    Sync from marxistutopia

--- lisp/fd-helm.el
+++ lisp/fd-helm.el
@@ -6,4 +6,15 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (setq helm-mode-fuzzy-match t)
+
+;; (defun helm-keyboard-quit ()
+;;   "Quit minibuffer in helm.
+;; If action buffer is displayed, kill it."
+;;   (interactive)
+;;   (with-helm-alive-p
+;;     (when (get-buffer-window helm-action-buffer 'visible)
+;;       (kill-buffer helm-action-buffer))
+;;     (setq helm-exit-status 1)
+;;     (condition-case nil (abort-recursive-edit) (error nil))))
+
 (provide 'fd-helm)
