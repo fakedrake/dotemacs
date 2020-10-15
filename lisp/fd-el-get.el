2a2cf167c19c64c406ed0aa8bb2b4b585274d2bd
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2020-10-15 10:08:34 +0100

    Removed package initi

--- lisp/fd-el-get.el
+++ lisp/fd-el-get.el
@@ -3,7 +3,6 @@
 (add-to-list 'load-path (concat dotemacs-dir "el-get/el-get/queue"))
 (setq el-get-dir (concat dotemacs-dir "el-get"))
 (require 'package)
-(package-initialize)
 
 (unless (require 'el-get nil t)
   (url-retrieve
@@ -16,6 +15,7 @@
 (setq my:el-get-packages
       '(;; Dependencies from who-kows-where
         flycheck
+        transient
 	memoize
         helm
         helm-swoop
